c3f92e3911e339601257c78c2d25dd777374b3ef
dsjack <dsjack89@gmail.com>
2020-04-20 16:50:36 -0700

    Adds second empty activity

---
+++ app/src/main/java/com/example/djackad340/FormSuccessActivity.java
@@ -0,0 +1,14 @@
+package com.example.djackad340;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_success);
+    }
+}
