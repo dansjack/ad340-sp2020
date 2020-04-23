10cf87e9451ec610b5eeafcc583ecbaf29f9a6e2
dsjack <dsjack89@gmail.com>
2020-04-23 12:45:39 -0700

    Adds test for screen orientation change

--- app/src/androidTest/java/com/example/djackad340/MainActivityTest.java
+++ app/src/androidTest/java/com/example/djackad340/MainActivityTest.java
@@ -1,9 +1,11 @@
 package com.example.djackad340;
 
+import android.os.RemoteException;
 import android.widget.DatePicker;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.uiautomator.UiDevice;
 
 import org.hamcrest.Matchers;
 import org.junit.Rule;
@@ -16,15 +18,11 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.contrib.PickerActions.setDate;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
@@ -76,6 +74,18 @@ public class MainActivityTest {
         onView(withId(android.R.id.button1)).perform(click());
 
         onView(withId(R.id.dobBtn)).check(matches(withText(Constants.TEST_DOB))); // Birthday
+        onView(withId(R.id.ageText)).check(matches(withText(Constants.TEST_AGE))); // Age
+    }
+
+    @Test
+    public void dataPersistsOnOrientationChange() throws RemoteException {
+        onView(withId(R.id.dobBtn)).perform(click()); // Enter Birthday
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(setDate(
+                Constants.TEST_YEAR, Constants.TEST_MONTH, Constants.TEST_DAY));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        device.setOrientationLeft();
         onView(withId(R.id.ageText)).check(matches(withText(Constants.TEST_AGE))); // Age
     }
 }
