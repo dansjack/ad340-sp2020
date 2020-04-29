b71448dc49b7cf3f9033460ccdbdc364a20421c9
dsjack <dsjack89@gmail.com>
2020-04-29 16:04:42 -0700

    Replaces rule in Intent Tests class

--- app/src/androidTest/java/com/example/djackad340/MainActivityTestIntent.java
+++ app/src/androidTest/java/com/example/djackad340/MainActivityTestIntent.java
@@ -2,9 +2,13 @@ package com.example.djackad340;
 
 import android.widget.DatePicker;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -27,8 +31,9 @@ import static org.hamcrest.core.AllOf.allOf;
 public class MainActivityTestIntent {
 
     @Rule
-    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
-            MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
 
     @Test
     public void verifyMessageSentToMessageActivity() {
@@ -46,9 +51,12 @@ public class MainActivityTestIntent {
         onView(withId(R.id.descText)) // Enter Description
                 .perform(typeText(Constants.TEST_DESCRIPTION), closeSoftKeyboard());
 
+        Intents.init();
         onView(withId(R.id.submitBtn)).perform(click());
         intended(hasExtra(Constants.KEY_FNAME, Constants.TEST_FNAME));
         intended(hasComponent(FormSuccessActivity.class.getName()));
+        Intents.release();
+
     }
 
     @Test
@@ -77,4 +85,5 @@ public class MainActivityTestIntent {
         onView(withId(R.id.ageText)).check(matches(withText(Constants.EMPTY_STRING)));
         onView(withId(R.id.descText)).check(matches(withText(Constants.EMPTY_STRING)));
     }
+
 }
